0779d7e1c35ed457e29835e3b7fb4f5de00146c3
Matt Lim <mlim@caltech.edu>
2017-07-25 17:04:07 -0700

    Project skeleton

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+  t.title = "dino-love"
+  t.console = true -- attach a console to the windows version
+  t.window.width = 800
+  t.window.height = 300
+end
---
+++ main.lua
@@ -0,0 +1,6 @@
+function love.load()
+end
+
+function love.draw()
+  love.graphics.print("hello world", 100, 100)
+end
